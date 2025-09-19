afa44afed2ddb59fb957102f1a0474891a17c65d
rusbel08 <cointendencia@gmail.com>
2025-09-18 21:36:10 -0500

    Cambio all

--- app/src/main/java/com/example/multiversofit/SqliteUsuario.java
+++ app/src/main/java/com/example/multiversofit/SqliteUsuario.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class SqliteUsuario extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "multiversofit.db";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 3;
     private static SqliteUsuario INSTANCE;
 
     public static synchronized SqliteUsuario get(Context ctx) {
